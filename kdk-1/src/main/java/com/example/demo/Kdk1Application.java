d9f07b0ded157a905c5390bb399a747481a58a30
kdkdinesh <dineshkumarkarri8618@gmail.com>
2021-08-16 10:56:42 +0530

    first commit

---
+++ kdk-1/src/main/java/com/example/demo/Kdk1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kdk1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kdk1Application.class, args);
+	}
+
+}
---
+++ kdk-2/src/test/java/com/example/demo/Kdk2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kdk2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
